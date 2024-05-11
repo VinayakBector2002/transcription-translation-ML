172c9a02df4a0be7ef0eda700b83874d0cfea61f
VinayakBector2002 <bectorjee@gmail.com>
2024-05-11 13:58:41 -0400

    basic layout added

---
+++ TT_ML/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex flex-col p-4">
+      <section className="min-h-screen flex flex-col">
+        <header>
+          <h1>One<span className="text-blue-400">ToMany</span></h1>
+        </header>
+        <main className="flex-1">
+        </main>
+        <footer> 
+        </footer>
+      </section>
+    </div>
+  )
+}
+
+export default App
