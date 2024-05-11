fda169977dbe8dc512e90ccc53fb1a56994df865
VinayakBector2002 <bectorjee@gmail.com>
2024-05-11 14:06:35 -0400

    New components added

--- TT_ML/src/App.jsx
+++ TT_ML/src/App.jsx
@@ -1,12 +1,14 @@
+import HomePage from "./components/HomePage"
+
 function App() {
   return (
-    <div className="flex flex-col p-4">
+    <div className="flex flex-col max-w-[1080px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-        <header>
+        <header className="flex items-center justify-between gap-4 p-4">
           <h1>One<span className="text-blue-400">ToMany</span></h1>
+          <button className="flex items-center gap-2">New<i className="fa-solid fa-plus"></i></button>
         </header>
-        <main className="flex-1">
-        </main>
+        <HomePage />
         <footer> 
         </footer>
       </section>
