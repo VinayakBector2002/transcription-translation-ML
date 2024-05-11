15745673e6024b8b95e2395e0c4d623ead89383d
VinayakBector2002 <bectorjee@gmail.com>
2024-05-11 14:07:50 -0400

    Header component added

--- TT_ML/src/App.jsx
+++ TT_ML/src/App.jsx
@@ -1,13 +1,11 @@
 import HomePage from "./components/HomePage"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="flex flex-col max-w-[1080px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-        <header className="flex items-center justify-between gap-4 p-4">
-          <h1>One<span className="text-blue-400">ToMany</span></h1>
-          <button className="flex items-center gap-2">New<i className="fa-solid fa-plus"></i></button>
-        </header>
+        <Header/>
         <HomePage />
         <footer> 
         </footer>
---
+++ TT_ML/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between gap-4 p-4">
+    <h1>One<span className="text-blue-400">ToMany</span></h1>
+    <button className="flex items-center gap-2">New<i className="fa-solid fa-plus"></i></button>
+  </header>
+  )
+}
